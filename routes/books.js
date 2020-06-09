914c5a27eb9c0d0a4313ea355ef8a13df091794e
japh <jenny.scherr@svenskaspel.se>
2020-06-09 13:47:27 +0200

    changed ref from users to books

---
+++ routes/books.js
@@ -0,0 +1,9 @@
+var express = require("express");
+var router = express.Router();
+
+/* GET users listing. */
+router.get("/", (req, res) => {
+  res.json({ books: [{ title: "The Bible" }] });
+});
+
+module.exports = router;
